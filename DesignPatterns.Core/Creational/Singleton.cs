2f88fc0a97babcbcea1572269d8e76ecbcf519dd
AkhmedovValikhan <ahmedoolo@mail.ru>
2016-06-19 16:17:26 +0300

    Initial

---
+++ DesignPatterns.Core/Creational/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Core.Creational
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object _syncObject = new object();
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                //Prevent redundant critical section enters
+                if (_instance == null)
+                {
+                    lock (_syncObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
